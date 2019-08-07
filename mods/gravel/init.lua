07f5b6b4a5d76fe6c8606b6f77dd44c28e505ad6
James Stevenson <jastevenson303@gmail.com>
2019-08-07 12:11:15 -0400

    Gravel

--- mods/gravel/init.lua
+++ mods/gravel/init.lua
@@ -1,1 +1,26 @@
+minetest.register_node("gravel:gravel", {
+	description = "Gravel",
+	tiles = {"gravel_gravel.png"},
+	groups = {crumbly = 2, falling_node = 1},
+	--sounds = default.node_sound_gravel_defaults(),
+	drop = {
+		max_items = 1,
+		items = {
+			{items = {"gravel:flint"}, rarity = 16},
+			{items = {"gravel:gravel"}}
+		}
+	}
+})
+
+minetest.register_craftitem("gravel:flint", {
+	description = "Flint",
+	inventory_image = "default_flint.png"
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "gravel:gravel 2",
+	recipe = {"stone:cobble"},
+})
+
 print("gravel loaded")
