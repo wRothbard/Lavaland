1041cdf5f70b9f02cda8361e66ade3c7d78f2c16
James Stevenson <james@she.din>
2019-08-10 22:26:23 -0400

    add ladders

---
+++ mods/ladders/init.lua
@@ -0,0 +1,70 @@
+minetest.register_node("ladders:ladder_wood", {
+	description = "Wooden Ladder",
+	drawtype = "signlike",
+	tiles = {"default_ladder_wood.png"},
+	inventory_image = "default_ladder_wood.png",
+	wield_image = "default_ladder_wood.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	sunlight_propagates = true,
+	walkable = false,
+	climbable = true,
+	is_ground_content = false,
+	selection_box = {
+		type = "wallmounted",
+		--wall_top = = <default>
+		--wall_bottom = = <default>
+		--wall_side = = <default>
+	},
+	groups = {choppy = 2, oddly_breakable_by_hand = 3, flammable = 2},
+	legacy_wallmounted = true,
+	--sounds = default.node_sound_wood_defaults(),
+})
+
+minetest.register_node("ladders:ladder_steel", {
+	description = "Steel Ladder",
+	drawtype = "signlike",
+	tiles = {"default_ladder_steel.png"},
+	inventory_image = "default_ladder_steel.png",
+	wield_image = "default_ladder_steel.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	sunlight_propagates = true,
+	walkable = false,
+	climbable = true,
+	is_ground_content = false,
+	selection_box = {
+		type = "wallmounted",
+		--wall_top = = <default>
+		--wall_bottom = = <default>
+		--wall_side = = <default>
+	},
+	groups = {cracky = 2},
+	--sounds = default.node_sound_metal_defaults(),
+})
+
+minetest.register_craft({
+	output = "ladders:ladder_wood 5",
+	recipe = {
+		{"group:stick", "", "group:stick"},
+		{"group:stick", "group:stick", "group:stick"},
+		{"group:stick", "", "group:stick"},
+	}
+})
+
+minetest.register_craft({
+	output = "ladders:ladder_steel 15",
+	recipe = {
+		{"steel:ingot", "", "steel:ingot"},
+		{"steel:ingot", "steel:ingot", "steel:ingot"},
+		{"steel:ingot", "", "steel:ingot"},
+	}
+})
+
+minetest.register_craft({
+	type = "fuel",
+	recipe = "ladder:ladder_wood",
+	burntime = 7,
+})
+
+print("loaded ladders")
