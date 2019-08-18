035cf730560258a9272c1edcda86c58f2e3817f9
James Stevenson <james@she.din>
2019-08-18 04:22:41 -0400

    add broken book

--- mods/books/init.lua
+++ mods/books/init.lua
@@ -1,5 +1,149 @@
 local lpp = 14 -- Lines per book's page
 
+local function on_place(itemstack, placer, pointed_thing)
+	if minetest.is_protected(pointed_thing.above, placer:get_player_name()) then
+		-- TODO: record_protection_violation()
+		return itemstack
+	end
+
+	local meta = itemstack:get_metadata()
+	local data = minetest.deserialize(meta)
+	local stack = ItemStack({name = "books:book_closed"})
+	if data and data.owner then
+		stack:set_metadata(meta)
+	end
+
+	local _, placed = minetest.item_place(stack, placer, pointed_thing)
+	if placed then
+		itemstack:take_item()
+	end
+	return itemstack
+end
+
+local function after_place_node(pos, placer, itemstack, pointed_thing)
+	local data = minetest.deserialize(itemstack:get_metadata())
+	if data then
+		local meta = minetest.get_meta(pos)
+		meta:set_string("title", data.title)
+		meta:set_string("text", data.text)
+		meta:set_string("owner", data.owner)
+		meta:set_string("text_len", data.text_len)
+		meta:set_string("page", data.page)
+		meta:set_string("page_max", data.page_max)
+		meta:set_string("infotext", data.title .. "\n\n" ..
+				"by " .. data.owner)
+	end
+end
+
+local function formspec_display(meta, player_name, pos)
+	-- Courtesy of minetest_game/mods/default/craftitems.lua
+	local title, text, owner = "", "", player_name
+	local page, page_max, lines, string = 1, 1, {}, ""
+
+	if meta:to_table().fields.owner then
+		title = meta:get_string("title")
+		text = meta:get_string("text")
+		owner = meta:get_string("owner")
+
+		for str in (text .. "\n"):gmatch("([^\n]*)[\n]") do
+			lines[#lines+1] = str
+		end
+
+		if meta:to_table().fields.page then
+			page = meta:to_table().fields.page
+			page_max = meta:to_table().fields.page_max
+
+			for i = ((lpp * page) - lpp) + 1, lpp * page do
+				if not lines[i] then break end
+				string = string .. lines[i] .. "\n"
+			end
+		end
+	end
+
+	local formspec
+	if owner == player_name then
+		formspec = "size[8,8]" ..
+			"field[0.5,1;7.5,0;title;Title:;" ..
+				minetest.formspec_escape(title) .. "]" ..
+			"textarea[0.5,1.5;7.5,7;text;Contents:;" ..
+				minetest.formspec_escape(text) .. "]" ..
+			"button_exit[2.5,7.5;3,1;save;Save]"
+	else
+		formspec = "size[8,8]" ..
+			"label[0.5,0.5;by " .. owner .. "]" ..
+			"tablecolumns[color;text]" ..
+			"tableoptions[background=#00000000;highlight=#00000000;border=false]" ..
+			"table[0.4,0;7,0.5;title;#FFFF00," .. minetest.formspec_escape(title) .. "]" ..
+			"textarea[0.5,1.5;7.5,7;;" ..
+				minetest.formspec_escape(string ~= "" and string or text) .. ";]" ..
+			"button[2.4,7.6;0.8,0.8;book_prev;<]" ..
+			"label[3.2,7.7;Page " .. page .. " of " .. page_max .. "]" ..
+			"button[4.9,7.6;0.8,0.8;book_next;>]"
+	end
+
+	minetest.show_formspec(player_name,
+			"books:book_" .. minetest.pos_to_string(pos), formspec)
+end
+
+local function on_rightclick(pos, node, clicker, itemstack, pointed_thing)
+	if node.name == "books:book_closed" then
+		node.name = "books:book_open"
+		minetest.swap_node(pos, node)
+		local meta = minetest.get_meta(pos)
+		meta:set_string("infotext",
+				meta:get_string("text"))
+	elseif node.name == "books:book_open" then
+		local player_name = clicker:get_player_name()
+		local meta = minetest.get_meta(pos)
+		formspec_display(meta, player_name, pos)
+	end
+end
+
+local function on_punch(pos, node, puncher, pointed_thing)
+	if node.name == "books:book_open" then
+		node.name = "books:book_closed"
+		minetest.swap_node(pos, node)
+		local meta = minetest.get_meta(pos)
+		if meta:get_string("owner") ~= "" then
+			meta:set_string("infotext",
+					meta:get_string("title") .. "\n\n" ..
+					"by " .. meta:get_string("owner"))
+		end
+	end
+end
+
+local function on_dig(pos, node, digger)
+	if minetest.is_protected(pos, digger:get_player_name()) then
+		-- TODO: record_protection_violation()
+		return false
+	end
+
+	local meta = minetest.get_meta(pos)
+	local data = {
+		title = meta:get_string("title"),
+		text = meta:get_string("text"),
+		owner = meta:get_string("owner"),
+		text_len = meta:get_int("text_len"),
+		page = meta:get_int("page"),
+		page_max = meta:get_int("page_max"),
+	}
+
+	local stack
+	if data.owner ~= "" then
+		stack = ItemStack({name = "books:book_written"})
+		--stack:set_metadata(minetest.serialize(data))
+		stack:get_meta():from_table(data)
+	else
+		stack = ItemStack({name = "books:book"})
+	end
+
+	local adder = digger:get_inventory():add_item("main", stack)
+	if adder then
+		minetest.item_drop(adder, digger, digger:getpos())
+	end
+	minetest.remove_node(pos)
+end
+
 local function book_on_use(itemstack, user)
 	local player_name = user:get_player_name()
 	local meta = itemstack:get_meta()
@@ -64,88 +208,176 @@ local max_title_size = 80
 local short_title_size = 35
 
 minetest.register_on_player_receive_fields(function(player, formname, fields)
-	if formname ~= "books:book" then return end
-	local inv = player:get_inventory()
-	local stack = player:get_wielded_item()
+	if formname == "books:book" then
+		local inv = player:get_inventory()
+		local stack = player:get_wielded_item()
 
-	if fields.save and fields.title and fields.text
-			and fields.title ~= "" and fields.text ~= "" then
-		local new_stack, data
-		if stack:get_name() ~= "books:book_written" then
-			local count = stack:get_count()
-			if count == 1 then
-				stack:set_name("books:book_written")
+		if fields.save and fields.title and fields.text
+				and fields.title ~= "" and fields.text ~= "" then
+			local new_stack, data
+			if stack:get_name() ~= "books:book_written" then
+				local count = stack:get_count()
+				if count == 1 then
+					stack:set_name("books:book_written")
+				else
+					stack:set_count(count - 1)
+					new_stack = ItemStack("books:book_written")
+				end
 			else
-				stack:set_count(count - 1)
-				new_stack = ItemStack("books:book_written")
+				data = stack:get_meta():to_table().fields
 			end
-		else
-			data = stack:get_meta():to_table().fields
-		end
 
-		if data and data.owner and data.owner ~= player:get_player_name() then
-			return
-		end
+			if data and data.owner and data.owner ~= player:get_player_name() then
+				return
+			end
 
-		if not data then data = {} end
-		data.title = fields.title:sub(1, max_title_size)
-		data.owner = player:get_player_name()
-		local short_title = data.title
-		-- Don't bother triming the title if the trailing dots would make it longer
-		if #short_title > short_title_size + 3 then
-			short_title = short_title:sub(1, short_title_size) .. "..."
-		end
-		data.description = "\""..short_title.."\" by "..data.owner
-		data.text = fields.text:sub(1, max_text_size)
-		data.text = data.text:gsub("\r\n", "\n"):gsub("\r", "\n")
-		data.page = 1
-		data.page_max = math.ceil((#data.text:gsub("[^\n]", "") + 1) / lpp)
+			if not data then data = {} end
+			data.title = fields.title:sub(1, max_title_size)
+			data.owner = player:get_player_name()
+			local short_title = data.title
+			-- Don't bother triming the title if the trailing dots would make it longer
+			if #short_title > short_title_size + 3 then
+				short_title = short_title:sub(1, short_title_size) .. "..."
+			end
+			data.description = "\""..short_title.."\" by "..data.owner
+			data.text = fields.text:sub(1, max_text_size)
+			data.text = data.text:gsub("\r\n", "\n"):gsub("\r", "\n")
+			data.page = 1
+			data.page_max = math.ceil((#data.text:gsub("[^\n]", "") + 1) / lpp)
 
-		if new_stack then
-			new_stack:get_meta():from_table({ fields = data })
-			if inv:room_for_item("main", new_stack) then
-				inv:add_item("main", new_stack)
+			if new_stack then
+				new_stack:get_meta():from_table({ fields = data })
+				if inv:room_for_item("main", new_stack) then
+					inv:add_item("main", new_stack)
+				else
+					minetest.add_item(player:get_pos(), new_stack)
+				end
 			else
-				minetest.add_item(player:get_pos(), new_stack)
+				stack:get_meta():from_table({ fields = data })
 			end
-		else
-			stack:get_meta():from_table({ fields = data })
+
+		elseif fields.book_next or fields.book_prev then
+			local data = stack:get_meta():to_table().fields
+			if not data or not data.page then
+				return
+			end
+
+			data.page = tonumber(data.page)
+			data.page_max = tonumber(data.page_max)
+
+			if fields.book_next then
+				data.page = data.page + 1
+				if data.page > data.page_max then
+					data.page = 1
+				end
+			else
+				data.page = data.page - 1
+				if data.page == 0 then
+					data.page = data.page_max
+				end
+			end
+
+			stack:get_meta():from_table({fields = data})
+			stack = book_on_use(stack, player)
 		end
 
-	elseif fields.book_next or fields.book_prev then
-		local data = stack:get_meta():to_table().fields
-		if not data or not data.page then
-			return
-		end
+		-- Update stack
+		player:set_wielded_item(stack)
+	elseif formname:sub(1, 13) == "books:book_" then
+		if fields.save and fields.title ~= "" and fields.text ~= "" then
+			local pos = minetest.string_to_pos(formname:sub(14))
+			local node = minetest.get_node(pos)
+			local meta = minetest.get_meta(pos)
 
-		data.page = tonumber(data.page)
-		data.page_max = tonumber(data.page_max)
+			meta:set_string("title", fields.title)
+			meta:set_string("text", fields.text)
+			meta:set_string("owner", player:get_player_name())
+			meta:set_string("infotext", fields.text)
+			meta:set_int("text_len", fields.text:len())
+			meta:set_int("page", 1)
+			meta:set_int("page_max", math.ceil((fields.text:gsub("[^\n]", ""):len() + 1) / lpp))
+		elseif fields.book_next or fields.book_prev then
+			local pos = minetest.string_to_pos(formname:sub(14))
+			local node = minetest.get_node(pos)
+			local meta = minetest.get_meta(pos)
 
-		if fields.book_next then
-			data.page = data.page + 1
-			if data.page > data.page_max then
-				data.page = 1
+			if fields.book_next then
+				meta:set_int("page", meta:get_int("page") + 1)
+				if meta:get_int("page") > meta:get_int("page_max") then
+					meta:set_int("page", 1)
+				end
+			elseif fields.book_prev then
+				meta:set_int("page", meta:get_int("page") - 1)
+				if meta:get_int("page") == 0 then
+					meta:set_int("page", meta:get_int("page_max"))
+				end
 			end
-		else
-			data.page = data.page - 1
-			if data.page == 0 then
-				data.page = data.page_max
-			end
-		end
 
-		stack:get_meta():from_table({fields = data})
-		stack = book_on_use(stack, player)
+			formspec_display(meta, player:get_player_name(), pos)
+		end
 	end
-
-	-- Update stack
-	player:set_wielded_item(stack)
 end)
+
+minetest.register_node("books:book_open", {
+	description = "Book Open (you hacker you!)",
+	inventory_image = "default_book.png",
+	tiles = {
+		"books_book_open_top.png",	-- Top
+		"books_book_open_bottom.png",	-- Bottom
+		"books_book_open_side.png",	-- Right
+		"books_book_open_side.png",	-- Left
+		"books_book_open_front.png",	-- Back
+		"books_book_open_front.png"	-- Front
+	},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	sunlight_propagates = true,
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.375, -0.47, -0.282, 0.375, -0.4125, 0.282}, -- Top
+			{-0.4375, -0.5, -0.3125, 0.4375, -0.47, 0.3125},
+		}
+	},
+	--groups = {attached_node = 1}, -- FIXME
+	on_punch = on_punch,
+	on_rightclick = on_rightclick,
+})
+
+minetest.register_node("books:book_closed", {
+	description = "Book Closed (you hacker you!)",
+	inventory_image = "default_book.png",
+	tiles = {
+		"books_book_closed_topbottom.png",	-- Top
+		"books_book_closed_topbottom.png",	-- Bottom
+		"books_book_closed_right.png",	-- Right
+		"books_book_closed_left.png",	-- Left
+		"books_book_closed_front.png^[transformFX",	-- Back
+		"books_book_closed_front.png"	-- Front
+	},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	sunlight_propagates = true,
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.25, -0.5, -0.3125, 0.25, -0.35, 0.3125},
+		}
+	},
+	groups = {oddly_breakable_by_hand = 3, dig_immediate = 2}, --, attached_node = 1}, -- FIXME
+	on_dig = on_dig,
+	on_rightclick = on_rightclick,
+	after_place_node = after_place_node,
+})
 
 minetest.register_craftitem("books:book", {
 	description = "Book",
 	inventory_image = "default_book.png",
 	groups = {book = 1, flammable = 3},
 	on_use = book_on_use,
+	--on_place = on_place,
 })
 
 minetest.register_craftitem("books:book_written", {
@@ -154,6 +386,7 @@ minetest.register_craftitem("books:book_written", {
 	groups = {book = 1, not_in_creative_inventory = 1, flammable = 3},
 	stack_max = 1,
 	on_use = book_on_use,
+	--on_place = on_place,
 })
 
 minetest.register_craft({
@@ -238,7 +471,7 @@ minetest.register_node("books:bookshelf", {
 	paramtype2 = "facedir",
 	is_ground_content = false,
 	groups = {choppy = 3, oddly_breakable_by_hand = 2, flammable = 3},
-	--sounds = default.node_sound_wood_defaults(),
+	sounds = music.sounds.nodes.wood,
 
 	on_construct = function(pos)
 		local meta = minetest.get_meta(pos)
