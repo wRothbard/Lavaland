c1c8a568ed6ed74e9589dd391966e1c1be3175db
James Stevenson <jastevenson303@gmail.com>
2019-09-02 06:27:25 -0400

    teams

---
+++ mods/log/init.lua
@@ -0,0 +1,4 @@
+function minetest.log(level, text)
+end
+
+print("loaded log")
---
+++ mods/teams/init.lua
@@ -0,0 +1,29 @@
+local players = {}
+
+minetest.register_on_leaveplayer(function(player)
+	players[player:get_player_by_name()] = nil
+end)
+
+minetest.register_chatcommand("team", {
+	params = "[set <name>]",
+	func = function(name, param)
+		if param == "" then
+			return true, players[name]
+		end
+		param = param:split(" ")
+		if param[1] == "show" then
+			local s = ""
+			for k, v in pairs(players) do
+				s = s .. v .. ","
+			end
+			s = s:sub(1, -2)
+			return true, s
+		elseif param[1] == "set" then
+			local n = param[2]:gsub("%W", "")
+			players[name] = n
+			return true, n
+		end
+	end,
+})
+
+print("loaded teams")
