042bc7083afaddc3fd444e81a9aa7ef8eb15ef26
James Stevenson <jastevenson303@gmail.com>
2019-09-14 08:58:17 -0400

    chat pages

--- mods/chat/init.lua
+++ mods/chat/init.lua
@@ -7,21 +7,42 @@ if stored_chat then
 end
 
 local save_timer = 0
+local players = {}
 
 local function save()
 	store:set_string("chat", minetest.serialize(chat))
 end
 
-local function show_chat(name)
+local function show_chat(name, page)
+	if page then
+		page = page * 100 - 99
+		if not chat[page] then
+			players[name] = 1
+			show_chat(name)
+			return
+		end
+	else
+		page = 1
+	end
 	local chatbox = ""
-	for i = 1, #chat do
+	for i = page, page + 99 do
+		if i > 1000 or not chat[i] then
+			break
+		end
 		chatbox = chatbox .. chat[i] .. "\n"
 	end
+	if chatbox == "" then
+		players[name] = 1
+		show_chat(name)
+		return
+	end
 	local chat_fs = "size[8,7.25]" ..
-		"field[0.06,7.12;6.89,1;chatsend;;]" ..
+		"field[0.06,7.12;5.6,1;chatsend;;]" ..
 		"textarea[0.08,-0.28;8.5,8.3;;;" .. chatbox .. "]" ..
 		"field_close_on_enter[chatsend;false]" ..
-		"image_button[6.54,6.89;0.83,0.83;chat_update.png;update;]" ..
+		"button[5.24,6.81;0.83,1;prevp;<]" ..
+		"button[5.84,6.81;0.83,1;nextp;>]" ..
+		"image_button[6.54,6.9;0.83,0.83;chat_update.png;update;]" ..
 		"button[7.24,6.81;1,1;ok;OK]" ..
 	""
 	minetest.show_formspec(name, "chat:chat", chat_fs)
@@ -48,22 +69,43 @@ minetest.register_chatcommand("chat", {
 
 minetest.register_on_player_receive_fields(function(player, formname, fields)
 	if formname == "" and fields.chat then
-		show_chat(player:get_player_name())
-	elseif formname == "chat:chat" and (fields.chatsend or fields.ok) then
 		local name = player:get_player_name()
-		if fields.chatsend == "" or fields.update then
+		players[name] = 1
+		show_chat(name)
+	elseif formname == "chat:chat" then
+		local name = player:get_player_name()
+		if (fields.ok and fields.chatsend == "") or fields.update then
+			players[name] = 1
 			show_chat(name)
-		else
+		elseif fields.nextp then
+			local np = players[name] + 1
+			if np > 10 then
+				np = 1
+			end
+			players[name] = np
+			show_chat(name, np)
+		elseif fields.prevp then
+			local pp = players[name] - 1
+			if pp < 1 then
+				pp = 10
+			end
+			players[name] = pp
+			show_chat(name, pp)
+		elseif fields.chatsend ~= "" and (fields.chatsend or fields.ok) then
 			local thing = minetest.formspec_escape(fields.chatsend)
 			add_chat(name, thing)
 			show_chat(name)
-			minetest.chat_send_all("<" .. name .. "> " .. thing)
+			minetest.chat_send_all("<" .. name .. "> " .. fields.chatsend)
 		end
 	end
 end)
 
 minetest.register_on_chat_message(function(name, message)
 	add_chat(name, minetest.formspec_escape(message))
+end)
+
+minetest.register_on_leaveplayer(function(player)
+	players[player:get_player_name()] = nil
 end)
 
 minetest.register_on_shutdown(function()
