e6091e243503c59be176224b001b76ed3e43cca2
James Stevenson <jastevenson303@gmail.com>
2019-09-26 12:15:45 -0400

    teams

--- mods/teams/init.lua
+++ mods/teams/init.lua
@@ -6,6 +6,42 @@ teams.get_team = function(name)
 	return players[name]
 end
 
+local function set_clothing(player, team)
+	local name = player:get_player_name()
+	local d_inv = minetest.get_inventory({type = "detached",
+			name = name .. "_clothing"})
+	local itemss = {}
+	for i = 1, d_inv:get_size("clothing") do
+		local stack = d_inv:get_stack("clothing", i)
+		if stack:get_count() > 0 then
+			table.insert(itemss, stack)
+			d_inv:set_stack("clothing", i, nil)
+			clothing:run_callbacks("on_unequip", player, i, stack)
+		end
+	end
+	local c = {"hat", "shirt", "pants", "cape"}
+	for i = 1, 4 do
+		local stack = ItemStack({name = "clothing:" .. c[i] .. "_" .. team})
+		d_inv:set_stack("clothing", i, stack)
+		clothing:run_callbacks("on_equip", player, i, stack)
+	end
+	clothing.save(player, d_inv)
+	clothing:set_player_clothing(player)
+	inventory.throw_inventory(player:get_pos(), itemss)
+end
+
+teams.set_team = function(name, team)
+	local player = minetest.get_player_by_name(name)
+	if not player then
+		return
+	end
+	if team == "red" or team == "blue" or
+			team == "green" then
+		set_clothing(player, team)
+	end
+	players[name] = team
+end
+
 minetest.register_on_leaveplayer(function(player)
 	players[player:get_player_name()] = nil
 end)
@@ -18,15 +54,10 @@ minetest.register_chatcommand("team", {
 		end
 		param = param:split(" ")
 		if param[1] == "show" then
-			local s = ""
-			for k, v in pairs(players) do
-				s = s .. v .. ","
-			end
-			s = s:sub(1, -2)
-			return true, "Your team is: " .. s
+			return true, "Your team is: " .. teams.get_team(name)
 		elseif param[1] == "set" then
 			local n = param[2]:gsub("%W", "")
-			players[name] = n
+			teams.set_team(name, n)
 			return true, "Your team is now set to " .. n
 		end
 	end,
