f649628604f42b55659c2dc27e521ddb7fe1d53a
James Stevenson <jastevenson303@gmail.com>
2019-10-17 18:04:22 -0400

    add steel hoe

--- mods/farming/hoes.lua
+++ mods/farming/hoes.lua
@@ -1,7 +1,15 @@
 farming.register_hoe(":farming:hoe_wood", {
 	description = "Wooden Hoe",
 	inventory_image = "farming_tool_woodhoe.png",
-	max_uses = 100,
+	max_uses = 20,
 	material = "group:wood",
-	groups = {hoe = 1, flammable = 2},
+	groups = {hoe = 1, flammable = 2, trade_value = 3},
+})
+
+farming.register_hoe(":farming:hoe_steel", {
+	description = "Wooden Hoe",
+	inventory_image = "farming_tool_steelhoe.png",
+	max_uses = 100,
+	material = "steel:ingot",
+	groups = {hoe = 1, trade_value = 4},
 })
