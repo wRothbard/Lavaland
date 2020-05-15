30225564164bc3231f9385307dcab971ce6d63ca
Walter Rothbard <walter.rothbard@gmail.com>
2020-05-14 21:38:21 -0500

    * Fix glass sound * Couple aliases, variables, and another sound added for another mod   I want to support down the road

--- mods/default/init.lua
+++ mods/default/init.lua
@@ -1,11 +1,12 @@
 default = {}
 
 default.node_sound_wood_defaults = function () return music.sounds.nodes.wood end
-default.node_sound_glass_defaults = function () return music.sounds.nodes.glass end
+default.node_sound_glass_defaults = function () return music.sounds.material.glass end
 default.node_sound_metal_defaults = function () return music.sounds.nodes.metal end
 default.node_sound_stone_defaults = function () return music.sounds.nodes.stone end
 default.node_sound_leaves_defaults = function () return music.sounds.nodes.leaves end
 default.node_sound_dirt_defaults = function () return music.sounds.nodes.dirt end
+default.node_sound_sand_defaults = function () return music.sounds.nodes.sand end
 
 minetest.register_alias("default:steel_ingot", "steel:ingot")
 minetest.register_alias("steel:sheet_metal", "steel:ingot")
@@ -21,5 +22,11 @@ minetest.register_alias("default:fence_junglewood", "fences:fence_wood")
 minetest.register_alias("default:fence_pine_wood", "fences:fence_wood")
 minetest.register_alias("default:stone_brick", "stone:brick")
 minetest.register_alias("default:stonebrick", "stone:brick")
+minetest.register_alias("default:dirt", "dirt:dirt")
+
+default.get_hotbar_bg = forms.get_hotbar_bg
+default.gui_bg     = ""
+default.gui_bg_img = ""
+default.gui_slots  = ""
 
 print("loaded default")
